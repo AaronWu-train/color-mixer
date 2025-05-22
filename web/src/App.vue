81113c41cb493641ac2785b57168182e67ab8471
AaronWu-train <aaronwu0218@gmail.com>
2025-05-22 23:05:34 +0800

    Change web structure for my purpose

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,19 +1,16 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
+<script setup></script>
 
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <h1>Hello World</h1>
     </div>
   </header>
 
   <main>
-    <TheWelcome />
+    <h2>Test</h2>
   </main>
 </template>
 
