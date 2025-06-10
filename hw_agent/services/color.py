485610748a45471923edb8df048e58b161113cc8
AaronWu-train <aaronwu0218@gmail.com>
2025-06-10 13:28:51 +0100

    rewwrwerwewerwewe

--- hw_agent/services/color.py
+++ hw_agent/services/color.py
@@ -12,7 +12,7 @@ async def getColor():
     normalized = normalize(raw)  # 0-1
     clear_removed = remove_clear_channel(normalized)  # 0-255
     rgb_calibrated = calibrate_rgb(clear_removed)  # 0-255
-    gammaed = gamma_correction(clear_removed)  # 0-255
+    gammaed = gamma_correction(rgb_calibrated)  # 0-255
     r, g, b = np.clip(clear_removed, 0, 255)
 
     return (round(r), round(g), round(b))
